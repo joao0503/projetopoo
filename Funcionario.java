a4c5172d2902b17ba2f9f7257916270fe23988e6
Yan Balbino Nogueira <111818340+YanBalbino@users.noreply.github.com>
2023-08-08 21:33:10 -0300

    classes funcionário e orçamento
    implementação preliminar das classes funcionário e orçamento

---
+++ Funcionario.java
@@ -0,0 +1,36 @@
+package Classes;
+
+public class Funcionario{
+    private String usuario;
+    private String senha;
+    private boolean isGerente;
+
+    public Funcionario(String usuario, String senha, boolean isGerente){
+        setUsuario(usuario);
+        setSenha(senha);
+        setGerente(isGerente);
+    }
+    //  fazer outro construtor para isGerente string e/ou para gerente já existente(?)
+
+    public String getUsuario(){ return this.usuario; }
+    public void setUsuario(String user){
+        if (user != null && !user.isEmpty() && user.length() > 3){
+            this.usuario = user;
+        }
+        else System.out.println("Usuário inválido, tente novamente");
+    }
+
+    public String getSenha(){ return this.senha;}
+    public void setSenha(String senha){
+        if (senha != null && !senha.isEmpty() && senha.length() > 4){
+            this.senha = senha;
+        }
+        else System.out.println("Senha inválida, tente novamente");
+    }
+
+    public String getGerente(){ return this.isGerente; }
+    public void setGerente(){
+        if (this.isGerente == false) this.isGerente = true;
+        else System.out.println("Ja é gerente!");
+    }
+}
