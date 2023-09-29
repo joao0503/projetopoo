1f63816d926e2e1320079f0c4212a7bdfa9dd788
YanBalbino <yanbnogueira@hotmail.com>
2023-09-29 14:10:30 -0300

    teste primeira tela

--- src/view/Teste.java
+++ src/view/Teste.java
@@ -1,6 +1,23 @@
 package view;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class Teste {
-    
+public class Teste extends Application{
+    public void start(Stage pS) throws Exception{
+        Label msg = new Label();
+        msg.setText("Hello World em JavaFX");
+
+        Scene cena = new Scene(msg, 500, 300);
+
+        pS.setTitle("Primeira tela");
+        pS.setScene(cena);
+        pS.show();
+    }
+
+    public static void main (String args[]){
+        launch();
+        System.out.println("pikaspikaspikas");
+    }
 }
