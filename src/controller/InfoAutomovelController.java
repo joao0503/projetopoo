eeb4f9aaca4f18fb23ab6db155bd73e94ef95ee5
YanBalbino <yanbnogueira@hotmail.com>
2023-10-13 19:10:56 -0300

    add controller de InfoAutomovel

---
+++ src/controller/InfoAutomovelController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class InfoAutomovelController {
+    @FXML TextField campoMarca;
+    @FXML TextField campoAno;
+    @FXML TextField campoCor;
+    @FXML TextField campoQuilometragem;
+    @FXML TextField campoProprietário;
+    @FXML TextField campoPlaca;
+    @FXML TableView tabelaServicos;
+
+}
