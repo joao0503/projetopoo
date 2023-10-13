18a68dc279a0ea12c1b2ef4ead86147538466e48
YanBalbino <yanbnogueira@hotmail.com>
2023-10-13 20:51:32 -0300

    att alguns botoes que faltaram no clientesControll

--- src/controller/TelaClientesController.java
+++ src/controller/TelaClientesController.java
@@ -6,22 +6,23 @@ import view.Telas;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 
+
+
 public class TelaClientesController extends TelaPrincipalController{
 
     @FXML private TextField searchBar;
     @FXML private TableView tabelaClientes;
     @FXML private Button botaoNovoCliente;
 
-    public void buscar(ActionEvent event){
+    public void infoCliente(ActionEvent event){
+        System.out.println("Info do cliente");
+    }   
+
+    public void removerCliente(ActionEvent event) {
+        
+    }   
+
+    public void addCliente(ActionEvent event){
 
     }
-
-    @FXML
-    public void addCliente(ActionEvent event) throws Exception{
-        Telas.telaAdicionarCliente();
-    }   
-
-    public void infoCliente(ActionEvent event){
-        // criar popup de info do cliente
-    }   
 }
