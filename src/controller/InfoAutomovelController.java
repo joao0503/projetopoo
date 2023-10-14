255b632f89590e16b171a3313e7ff1458c642ba9
YanBalbino <yanbnogueira@hotmail.com>
2023-10-13 21:00:26 -0300

    comment funcoes a fazer em infoAutomovelController

--- src/controller/InfoAutomovelController.java
+++ src/controller/InfoAutomovelController.java
@@ -13,4 +13,9 @@ public class InfoAutomovelController {
     @FXML TextField campoPlaca;
     @FXML TableView tabelaServicos;
 
+    //método de editar info do automovel
+    //método de editar serviço na lista
+    //método de excluir serviço na lista
+    //método de adicionar serviço
+
 }
